c2b312243d9c3058db7e9bdfe93116d3eaaaf8c7
Jorge Cisneros <jorgecis@gmail.com>
2018-07-07 12:33:04 -0500

    Save last changes

--- devicetypes/jorgecis/esp32-switch.src/esp32-switch.groovy
+++ devicetypes/jorgecis/esp32-switch.src/esp32-switch.groovy
@@ -16,54 +16,34 @@
  */
  metadata {
  	definition (name: "ESP32 Switch", namespace: "jorgecis", author: "Jorge Cisneros") {
-        capability "Actuator"
         capability "Switch"
-        capability "Polling"
         capability "Refresh"
-        capability "Sensor"
-
-        attribute "currentIP", "string"
-
-        command "subscribe"
-        command "resubscribe"
-        command "unsubscribe"
+        capability "Button"
+        
+        command "push1"
         command "setOffline"
- }
+    }
 
- // simulator metadata
- simulator {}
+    // simulator metadata
+    simulator {}
+ 
 
- // UI tile definitions
-    tiles(scale: 2) {
-        multiAttributeTile(name:"rich-control", type: "switch", canChangeIcon: true){
-            tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-                 attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.off", backgroundColor:"#79b821", nextState:"turningOff"
-                 attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.switch.on", backgroundColor:"#ffffff", nextState:"turningOn"
-                 attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.switch.off", backgroundColor:"#79b821", nextState:"turningOff"
-                 attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.switch.on", backgroundColor:"#ffffff", nextState:"turningOn"
-                 attributeState "offline", label:'${name}', icon:"st.switches.switch.off", backgroundColor:"#ff0000"
- 			}
-            tileAttribute ("currentIP", key: "SECONDARY_CONTROL") {
-             	 attributeState "currentIP", label: ''
- 			}
+
+    // UI tile definitions
+    tiles {
+       standardTile("contact", "", width: 3, height: 2,  decoration: "ring") {
+			state "default", icon: "st.doors.garage.garage-closed", action: "push1", label: "toggle"
+		}
+        valueTile("ipaddress", "device.floatAsText", width: 3, height: 1) {
+        	state "inst", label: ""
         }
 
-        standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-            state "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.off", backgroundColor:"#79b821", nextState:"turningOff"
-            state "off", label:'${name}', action:"switch.on", icon:"st.switches.switch.on", backgroundColor:"#ffffff", nextState:"turningOn"
-            state "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.switch.off", backgroundColor:"#79b821", nextState:"turningOff"
-            state "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.switch.on", backgroundColor:"#ffffff", nextState:"turningOn"
-            state "offline", label:'${name}', icon:"st.switches.switch.off", backgroundColor:"#ff0000"
-        }
-
-        standardTile("refresh", "device.switch", inactiveLabel: false, height: 2, width: 2, decoration: "flat") {
-            state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
-        }
-
-        main(["switch"])
-        details(["rich-control", "refresh"])
+		main "contact"
+		details(["contact", "ipaddress"])
     }
 }
+
+
 
 // parse events into attributes
 def parse(String description) {
@@ -96,9 +76,6 @@ def parse(String description) {
  result
 }
 
-private getCallBackAddress() {
- 	device.hub.getDataValue("localIP") + ":" + device.hub.getDataValue("localSrvPortTCP")
-}
 
 private Integer convertHexToInt(hex) {
  	Integer.parseInt(hex,16)
@@ -131,134 +108,36 @@ private getHostAddress() {
     }
 }
 
-def on() {
-log.debug "Executing 'on'"
-    def result = new physicalgraph.device.HubAction(
-        method: "GET",
-        path: "/on",
-        headers: [
-            HOST: getHostAddress()
-        ]
-    )
-    return result
-}
-
-def off() {
-log.debug "Executing 'off'"
-    def result = new physicalgraph.device.HubAction(
-        method: "GET",
-        path: "/off",
-        headers: [
-            HOST: getHostAddress()
-        ]
-    )
-    log.debug result
-    return result
-}
-
-private subscribeAction(path, callbackPath="") {
-    log.trace "subscribe($path, $callbackPath)"
-    def address = getCallBackAddress()
-    def ip = getHostAddress()
-
-    def result = new physicalgraph.device.HubAction(
-        method: "SUBSCRIBE",
-        path: path,
-        headers: [
-            HOST: ip,
-            CALLBACK: "<http://${address}/notify$callbackPath>",
-            NT: "upnp:event",
-            TIMEOUT: "Second-28800"
-        ]
-    )
-
-    log.trace "SUBSCRIBE $path"
-
-    return result
-}
-
-private unsubscribeAction(path, callbackPath="") {
-    log.trace "unsubscribe($path, $callbackPath)"
-    def address = getCallBackAddress()
-    def ip = getHostAddress()
-
-    def result = new physicalgraph.device.HubAction(
-        method: "UNSUBSCRIBE",
-        path: path,
-        headers: [
-            HOST: ip,
-            CALLBACK: "<http://${address}/notify$callbackPath>",
-            NT: "upnp:event",
-            TIMEOUT: "Second-28800"
-        ]
-    )
-
-    log.trace "SUBSCRIBE $path"
-
-    return result
-}
-
-def subscribe() {
-	subscribeAction("/on")
-	subscribeAction("/off")
-	subscribeAction("/poll")
-}
 
 def refresh() {
- 	log.debug "Executing ESP8266 Switch 'subscribe', then 'poll'"
- 	[subscribe(), poll()]
 }
-
-def subscribe(ip, port) {
-    def existingIp = getDataValue("ip")
-    def existingPort = getDataValue("port")
-    if (ip && ip != existingIp) {
-         log.debug "Updating ip from $existingIp to $ip"    
-    	 updateDataValue("ip", ip)
-    	 def ipvalue = convertHexToIP(getDataValue("ip"))
-         sendEvent(name: "currentIP", value: ipvalue, descriptionText: "IP changed to ${ipvalue}")
-    }
- 	if (port && port != existingPort) {
- 		log.debug "Updating port from $existingPort to $port"
- 		updateDataValue("port", port)
-	}
-    
- 	log.debug "Subscribe ${ip}:${port}"
-	subscribe("${ip}:${port}")
-}
-
-def resubscribe() {
-    log.debug "Executing 'resubscribe()'"
-    subscribe()
-}
-
-
-def unsubscribe() {
-	unsubscribeAction("/on")
-	unsubscribeAction("/off")
-	unsubscribeAction("/poll")
-}
-
-
-//TODO: Use UTC Timezone
-
 
 def setOffline() {
 	//sendEvent(name: "currentIP", value: "Offline", displayed: false)
     sendEvent(name: "switch", value: "offline", descriptionText: "The device is offline")
 }
 
-def poll() {
-log.debug "Executing 'poll'"
-if (device.currentValue("currentIP") != "Offline")
-    runIn(30, setOffline)
 
-    def result = new physicalgraph.device.HubAction(
-        method: "GET",
-        path: "/poll",
-        headers: [
-            HOST: getHostAddress()
-        ]
-    )
-    
+
+def sync(ip, port) {
+    log.debug(ip)
+	def existingIp = getDataValue("ip")
+	def existingPort = getDataValue("port")
+	if (ip && ip != existingIp) {
+		updateDataValue("ip", ip)
+	}
+	if (port && port != existingPort) {
+		updateDataValue("port", port)
+	}
+}
+
+def push1() {
+    log.debug "Button pressed"
+    def httpRequest = [
+          	method:		"GET",
+            path: 		"/toogle",
+            headers:	[HOST: getHostAddress()]
+    ]
+  	def hubAction = new physicalgraph.device.HubAction(httpRequest)
+   	sendHubCommand(hubAction)
 }
